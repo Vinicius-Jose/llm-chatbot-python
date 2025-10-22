82bc032aac0452aacb7fc9f0cdaeafd55e3073ee
Vinicius José <vinijosenog@hotmail.com>
2025-10-22 13:02:00 -0300

    Add cypher prompt

--- prompts.py
+++ prompts.py
@@ -60,3 +60,28 @@ retrieval_prompt = ChatPromptTemplate.from_messages(
         ("human", "{input}"),
     ]
 )
+
+
+CYPHER_GENERATION_TEMPLATE = """
+You are an expert Neo4j Developer translating user questions into Cypher to answer questions about movies and provide recommendations.
+Convert the user's question based on the schema.
+
+Use only the provided relationship types and properties in the schema.
+Do not use any other relationship types or properties that are not provided.
+
+Do not return entire nodes or embedding properties.
+
+Fine Tuning:
+
+For movie titles that begin with "The", move "the" to the end. For example "The 39 Steps" becomes "39 Steps, The" or "the matrix" becomes "Matrix, The".
+
+
+Schema:
+{schema}
+
+Question:
+{question}
+
+Cypher Query:
+"""
+cypher_prompt = PromptTemplate.from_template(CYPHER_GENERATION_TEMPLATE)
--- tools/cypher.py
+++ tools/cypher.py
@@ -2,11 +2,13 @@ import streamlit as st
 from llm import llm
 from graph import graph
 from langchain_neo4j import GraphCypherQAChain
+from prompts import cypher_prompt
 
 # Create the Cypher QA chain
 cypher_qa = GraphCypherQAChain.from_llm(
     llm,
     graph=graph,
     verbose=True,
+    cypher_prompt=cypher_prompt,
     allow_dangerous_requests=True,
 )
