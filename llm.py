d73e7562ac0c3a1d49c686235e3a4465fbd2b71b
Vinicius José <vinijosenog@hotmail.com>
2025-10-22 12:22:51 -0300

    Ollama model from secret

--- llm.py
+++ llm.py
@@ -2,7 +2,9 @@ import streamlit as st
 from langchain_ollama import ChatOllama, OllamaEmbeddings
 
 # Create the LLM
-llm = ChatOllama(model="llama3.1")
+llm = ChatOllama(model=st.secrets.get("LLM_MODEL", "llama3.1"))
 
 # Create the Embedding model
-embeddings = OllamaEmbeddings(model="rjmalagon/gte-qwen2-1.5b-instruct-embed-f16")
+embeddings = OllamaEmbeddings(
+    model=st.secrets.get("EMBEDDINGS", "rjmalagon/gte-qwen2-1.5b-instruct-embed-f16")
+)
