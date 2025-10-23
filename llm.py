c41a9c1e1c7dd086e7e7b00f61833565efc7175d
Vinicius José <vinijosenog@hotmail.com>
2025-10-23 13:49:28 -0300

    Add Groq

--- llm.py
+++ llm.py
@@ -1,8 +1,11 @@
 import streamlit as st
 from langchain_ollama import ChatOllama, OllamaEmbeddings
+from langchain_groq import ChatGroq
 
 # Create the LLM
 llm = ChatOllama(model=st.secrets.get("LLM_MODEL", "llama3.1"))
+if st.secrets.get("GROQ_MODEL"):
+    llm = ChatGroq(model=st.secrets.get("GROQ_MODEL", "deepseek-r1-distill-llama-70b"))
 
 # Create the Embedding model
 embeddings = OllamaEmbeddings(
